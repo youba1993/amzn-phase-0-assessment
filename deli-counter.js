46cc4f3fb7793c42835c9db649198482c90118ee
youbaidjis <youba.etud@gmail.com>
2022-04-09 00:41:36 -0700

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,5 +1,36 @@
 // 1. Write your functions here
 
+const katzDeli = [];
+function takeANumber(arr, name){
+    arr.push(name)                 //add a new name to the end of the array 
+    console.log( `Welcome, ${name}. You are number ${arr.length} in line.`); 
+}
+takeANumber(katzDeli, "ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+takeANumber(katzDeli, "youba")
+takeANumber(katzDeli, "fouzia")
+console.log(katzDeli)  
+
+function line(arr){
+    const lineKey = []      //push the element and there index to an array and output the result 
+    arr.forEach(element => { lineKey.push(`${arr.indexOf(element)+1}. ${element} ` )})
+    
+     console.log(` The line is currently : ${lineKey}` )
+      
+        
+}
+line(katzDeli)
+function nowServing(arr){        
+        console.log(`Currently serving ${arr[0]}.`)
+        arr.shift()     //delete the first element after serving
+}
+nowServing(katzDeli)
+line(katzDeli)
+takeANumber(katzDeli, "Matz")
+line(katzDeli)
+nowServing(katzDeli)
+line(katzDeli)
 
 // 2. Example Usage
 
